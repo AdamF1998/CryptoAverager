66ffa031636ee257a31bd0bcc982e87851186e85
Adam Ferguson <adamferguson16@hotmail.com>
2021-08-22 23:59:51 +0100

    Replaced the binance logo.
    Implemented a simple form with a checkbox for each exchange.
    
    Styling needs to be changed but the functionality is there...
    
    Next step:
    OnSubmit() needs to take the user to the next page to allow api access to the exchanges..

---
+++ CryptoAverager.Front/RazorModels/ExchangeRazorModel.cs
@@ -0,0 +1,9 @@
+﻿namespace CryptoAverager.Front.RazorModels
+{
+    public class ExchangeRazorModel
+    {
+        public bool IsBinanceSelected { get; set; }
+
+        public bool IsHuobiSelected { get; set; }
+    }
+}
