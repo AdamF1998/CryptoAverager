5cc68d206f692b176cb4714026091ab26579db0b
Adam Ferguson <adamferguson16@hotmail.com>
2021-08-27 21:08:52 +0100

    Did something

--- CryptoAverager.FrontEnd/RazorModels/HuobiAuthenticationRazorModel.cs
+++ CryptoAverager.FrontEnd/RazorModels/HuobiAuthenticationRazorModel.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace CryptoAverager.FrontEnd.RazorModels
+﻿namespace CryptoAverager.FrontEnd.RazorModels
 {
     public class HuobiAuthenticationRazorModel
     {
